d294ac62bf09659bd697987ed761376599b9ed5c
Kevin Trinanda <56400686+Kevintrinanda@users.noreply.github.com>
2019-10-10 21:52:13 +0700

    Add files via upload

---
+++ PBO10117008Latihan18/src/pbo10117008latihan18/PBO10117008Latihan18.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10117008latihan18;
+
+/**
+ *
+ * @author ASUS
+ * Nama         : Kevin Trinanda
+ * Kelas        : IF-1 V
+ * NIM          : 10117008
+ * Deskripsi    : Program ini akan membantu kita menentukan kelipatan angka
+ */
+public class PBO10117008Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      float i= (float) 3.5;
+        while (i<=35)
+        {
+            if (i%3!=0)
+        {
+            System.out.println(i);
+        }
+            i+=3.5;
+        }
+    }
+    
+}
